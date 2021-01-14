9788fa17b4ded92180454a76d68fc330fbbb119d
“brunobatista” <brunobatista101@gmail.com>
2021-01-14 17:01:36 -0300

    ajustes requisito 1

--- tests/test_file_mangement.py
+++ tests/test_file_mangement.py
@@ -12,13 +12,13 @@ def test_validar_importar_noticias_com_sucesso(capsys):
     assert txt_importer("statics/arquivo_teste.txt") == text_file
 
 
-def test_validar_com_extencao_invalida(capsys):
+def test_validar_importar_com_extencao_invalida(capsys):
     txt_importer("statics/arquivo_teste.csv")
     out, err = capsys.readouterr()
     assert err == "Formato inválido\n"
 
 
-def test_validar_com_arquivo_nao_existente(capsys):
+def test_validar_importar_com_arquivo_nao_existente(capsys):
     txt_importer("statics/arquivo_nao_existe.txt")
     out, err = capsys.readouterr()
     assert err == "Arquivo statics/arquivo_nao_existe.txt não encontrado\n"
