4605b29cc52430a303b1047b8184b43b5c50b742
“brunobatista” <brunobatista101@gmail.com>
2021-01-19 11:47:36 -0300

    ajuste arquiv

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -2,23 +2,12 @@ from ting_file_management.file_management import txt_importer
 import sys
 
 def process(path_file, instance):
-    result = txt_importer(path_file)
-    returned_object = {
-        "nome_do_arquivo": path_file,
-        "qtd_linhas": len(result),
-        "linhas_do_arquivo": result,
-    }
-    instance.push_back(returned_object)
+    raise NotImplementedError
 
 
 def remove(instance):
-    instance.pop_front()
+    raise NotImplementedError
 
 
 def file_metadata(instance, position):
-    item = instance.peek_by_position(position)
-    if item == 'Posição inválida':
-        print('Posição inválida', file=sys.stderr)
-        return
-    print(item)
-    return
+    raise NotImplementedError
